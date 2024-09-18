0f28cbfe32ead106723514e59dfc76c294b1f5d7
learnhl7-arun <learhhl7.arun@gmail.com>
2024-09-18 06:51:50 +0530

    Processed Bulk Data and email sent

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ import schedule from 'node-schedule'
 const tokenEndpoint = "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token"
 const fhirBaseUrl = "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4"
 const groupId = "e3iabhmS8rsueyz7vaimuiaSmfGvi.QwjVXJANlPOgR83"
+const clientId = process.env.EPIC_BACKEND_APP_CLIENT_ID
 
 function clearScreen() {
     process.stdout.write('\x1B[2J\x1B[0f');
@@ -78,7 +79,7 @@ const pollAndWaitForExport = async (url, accessToken, secsToWait = 5) => {
         const progress = response.headers.get("X-Progress")
         const status = response.status
         const data = response.data
-        
+
         if (status == 200) {
             return data
         }
@@ -94,13 +95,102 @@ const pollAndWaitForExport = async (url, accessToken, secsToWait = 5) => {
     return await pollAndWaitForExport(url, accessToken, secsToWait)
 }
 
-clearScreen()
-//Get the client ID from the environment
-const clientId = process.env.EPIC_BACKEND_APP_CLIENT_ID
-// console.log('Client ID', clientId)
+const processBulkResponse = async (bundleResponse, accessToken, type, fn) => {
+    const filteredOutputs = bundleResponse.output?.filter((output) => output.type == type)
+    const promises = filteredOutputs?.map((output) => {
+        const url = output.url
+        return new Promise((resolve) => {
+            const stream = hyperquest(url, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            });
 
-const tokenResponse = await makeTokenRequest()
-const accessToken = tokenResponse.access_token
-const contentLocation = await kickOffBulkDataExport(accessToken)
-const bulkDataResponse = await pollAndWaitForExport(contentLocation, accessToken,3)
-console.log(bulkDataResponse, accessToken)
+            stream.pipe(ndjson.parse()).on('data', fn)
+            stream.on('error', resolve)
+            stream.on('end', resolve)
+        })
+    })
+    return await Promise.all(promises)
+}
+
+const checkIfObservationIsNormal = (resource) => {
+    const value = resource?.valueQuantity?.value
+    if (!resource?.referenceRange) {
+        return { isNormal: false, reason: "No reference range found" }
+    }
+    const referenceRangeLow = resource?.referenceRange?.[0]?.low?.value
+    const referenceRangeHigh = resource?.referenceRange?.[0]?.high?.value
+    if (!value || !referenceRangeLow || !referenceRangeHigh) {
+        return { isNormal: false, reason: "Incomplete data" }
+    }
+    if (value >= referenceRangeLow && value <= referenceRangeHigh) {
+        return { isNormal: true, reason: "Within reference range" }
+    } else {
+        return { isNormal: false, reason: "Outside reference range" }
+    }
+}
+
+const sendEmail = async (body) => {
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+            user: 'rory.schimmel@ethereal.email',
+            pass: 'rPfnw2v1q1wWNWSPKk'
+        }
+    });
+
+    return await transporter.sendMail(body)
+}
+
+const main = async () => {
+    //
+    clearScreen()
+    console.log("Running main function")
+
+    const tokenResponse = await makeTokenRequest()
+    const accessToken = tokenResponse.access_token
+    const contentLocation = await kickOffBulkDataExport(accessToken)
+    const bulkDataResponse = await pollAndWaitForExport(contentLocation, accessToken, 3)
+    // console.log(bulkDataResponse, accessToken)
+
+    // Fetch Patient Data from bulk data response
+    const patients = {}
+    await processBulkResponse(bulkDataResponse, accessToken, 'Patient', (resource)=>{
+    patients[`Patient/${resource.id}`] = resource})
+
+
+    // Fetch Observation Data and construct email message
+    let message = `Results of lab tests in sandbox (Date: ${new Date().toISOString()})\n`
+    let abnormalObservations = ``
+    let normalObservations = ``
+    await processBulkResponse(bulkDataResponse, accessToken, 'Observation', (resource)=>{
+        const {isNormal, reason} = checkIfObservationIsNormal(resource)
+        const patient = patients[resource.subject.reference]
+        if (isNormal) {
+        normalObservations += `${resource.code.text}: ${resource?.valueQuantity?.value}. Reason: ${reason}, Patient Name: ${patient?.name?.[0]?.text}, Patient ID: ${patient?.id}\n`
+        } else {
+        abnormalObservations += `${resource.code.text}. Reason: ${reason}. Patient Name: ${patient?.name?.[0]?.text}, Patient ID: ${patient?.id}\n`
+        }
+    })
+
+    message += 'Abnormal Observations:\n' + abnormalObservations + '\n\n'
+    message += 'Normal Observations:\n' + normalObservations
+
+    console.log(message)
+
+    const emailAck = await sendEmail({
+        from: '"Auto Process" <autorun@test-hospital.com>', // sender address
+        to: "practitioner@test-hospital.com", // list of receivers
+        subject: `Lab Reports on ${new Date().toDateString()} 🔥`, // Subject line
+        text: message, // html body
+      })
+      console.log("Email sent", emailAck)
+}
+
+main()
+
+
+
+
